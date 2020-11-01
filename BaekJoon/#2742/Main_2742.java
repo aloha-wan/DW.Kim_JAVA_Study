0d4f63a107bbda6b406454d6f63ec39596b73116
aloha0319 <50447511+aloha0319@users.noreply.github.com>
2020-11-01 22:15:43 +0900

    20201101

---
+++ BaekJoon/#2742/Main_2742.java
@@ -0,0 +1,30 @@
+package DW_Kim_JAVA_Study_BaekJoon;
+
+import java.util.Scanner;
+
+/**
+ * 작  성  자 : Kim Do Wan
+ * 일       자 : 2020. 11. 1.
+ * 문제 제목 : 기찍 N
+ * 문제 번호 : 2742
+ * 문제 출처 : https://www.acmicpc.net/
+ * 
+ * 문       제 : 자연수 N이 주어졌을 때, N부터 1까지 한 줄에 하나씩 출력하는 프로그램을 작성하시오.
+ *
+ * 입       력 : 첫째 줄에 100,000보다 작거나 같은 자연수 N이 주어진다.
+ *
+ * 출       력 : 첫째 줄부터 N번째 줄 까지 차례대로 출력한다.
+ *
+ * 
+ **/
+public class Main_2742 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        
+        for(int i = N; i > 0; i--) {
+	        System.out.println(i);
+        }
+    }
+}
