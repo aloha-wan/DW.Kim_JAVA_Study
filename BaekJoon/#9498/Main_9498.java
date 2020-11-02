45b1d19c6886937245ec48215bca90ee7621ebde
aloha0319 <50447511+aloha0319@users.noreply.github.com>
2020-11-02 16:47:58 +0900

    20201102

---
+++ BaekJoon/#9498/Main_9498.java
@@ -0,0 +1,41 @@
+package DW_Kim_JAVA_Study_BaekJoon;
+
+import java.util.Scanner;
+
+/**
+ * 작  성  자 : Kim Do Wan
+ * 일       자 : 2020. 11. 2.
+ * 문제 제목 : 시험 성적
+ * 문제 번호 : 9498
+ * 문제 출처 : https://www.acmicpc.net/
+ * 
+ * 문       제 : 시험 점수를 입력받아 90 ~ 100점은 A, 80 ~ 89점은 B, 70 ~ 79점은 C, 60 ~ 69점은 D, 나머지 점수는 F를 출력하는 프로그램을 작성하시오.
+ *
+ * 입       력 : 첫째 줄에 시험 점수가 주어진다. 시험 점수는 0보다 크거나 같고, 100보다 작거나 같은 정수이다.
+ *
+ * 출       력 : 시험 성적을 출력한다.
+ *
+ * 
+ **/
+public class Main_9498 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int grade = sc.nextInt();
+		
+		if(90 <= grade && grade <= 100){
+			System.out.println("A");
+		} else if(80 <= grade && grade < 90){
+			System.out.println("B");
+		}  else if(70 <= grade && grade < 80){
+			System.out.println("C");
+		}  else if(60 <= grade && grade < 70){
+			System.out.println("D");
+		} else {
+			System.out.println("F");
+		}
+
+	}
+
+}
