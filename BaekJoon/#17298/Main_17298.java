3562237dd6e26b2d4e17b11546fc6a4e372c64eb
aloha0319 <50447511+aloha0319@users.noreply.github.com>
2020-11-16 22:30:00 +0900

    20201116

---
+++ BaekJoon/#17298/Main_17298.java
@@ -0,0 +1,67 @@
+package DW_Kim_JAVA_Study_BaekJoon;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 작  성  자 : Kim Do Wan
+ * 일       자 : 2020. 11. 16.
+ * 문제 제목 : 오큰수
+ * 문제 번호 : 17298
+ * 문제 출처 : https://www.acmicpc.net/
+ * 
+ * 문       제 : 크기가 N인 수열 A = A1, A2, ..., AN이 있다. 수열의 각 원소 Ai에 대해서 오큰수 NGE(i)를 구하려고 한다. 
+ *          Ai의 오큰수는 오른쪽에 있으면서 Ai보다 큰 수 중에서 가장 왼쪽에 있는 수를 의미한다. 그러한 수가 없는 경우에 오큰수는 -1이다.
+ *          예를 들어, A = [3, 5, 2, 7]인 경우 NGE(1) = 5, NGE(2) = 7, NGE(3) = 7, NGE(4) = -1이다. 
+ *          A = [9, 5, 4, 8]인 경우에는 NGE(1) = -1, NGE(2) = 8, NGE(3) = 8, NGE(4) = -1이다.
+ *
+ * 입       력 : 첫째 줄에 수열 A의 크기 N (1 ≤ N ≤ 1,000,000)이 주어진다. 둘째에 수열 A의 원소 A1, A2, ..., AN (1 ≤ Ai ≤ 1,000,000)이 주어진다.
+ *
+ * 출       력 : 총 N개의 수 NGE(1), NGE(2), ..., NGE(N)을 공백으로 구분해 출력한다.
+ *
+ * 
+ **/
+public class Main_17298 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(bf.readLine());
+        
+        int[] a = new int[n];
+        int[] ans = new int[n];
+        int[] freq = new int[1000001];
+        
+        String[] temp = bf.readLine().split(" ");
+        Stack<Integer> s = new Stack<>();
+        
+        for (int i=0; i<n; i++) {
+            a[i] = Integer.parseInt(temp[i]);
+            freq[a[i]] += 1;
+        }
+        
+        s.push(0);
+        
+        for (int i=1; i<n; i++) {
+            if (s.isEmpty()) {
+                s.push(i);
+            }
+            while (!s.isEmpty() && freq[a[s.peek()]] < freq[a[i]]) {
+                ans[s.pop()] = a[i];
+            }
+            s.push(i);
+        }
+        
+        while (!s.empty()) {
+            ans[s.pop()] = -1;
+        }
+        
+        for (int i=0; i<n; i++) {
+            bw.write(ans[i] + " ");
+        }
+        
+        bw.write("\n");
+        bw.flush();
+    }
+}
