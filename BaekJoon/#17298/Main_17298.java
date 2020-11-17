65c9d7e8a7b843f6bf4f36489de76964852afa04
aloha0319 <50447511+aloha0319@users.noreply.github.com>
2020-11-17 13:31:19 +0900

    20201117

--- BaekJoon/#17298/Main_17298.java
+++ BaekJoon/#17298/Main_17298.java
@@ -25,29 +25,24 @@ public class Main_17298 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        int n = Integer.parseInt(bf.readLine());
         
+        int n = Integer.parseInt(bf.readLine());
         int[] a = new int[n];
         int[] ans = new int[n];
-        int[] freq = new int[1000001];
-        
         String[] temp = bf.readLine().split(" ");
-        Stack<Integer> s = new Stack<>();
         
         for (int i=0; i<n; i++) {
             a[i] = Integer.parseInt(temp[i]);
-            freq[a[i]] += 1;
         }
         
+        Stack<Integer> s = new Stack<>();
         s.push(0);
         
         for (int i=1; i<n; i++) {
             if (s.isEmpty()) {
                 s.push(i);
             }
-            while (!s.isEmpty() && freq[a[s.peek()]] < freq[a[i]]) {
+            while (!s.isEmpty() && a[s.peek()] < a[i]) {
                 ans[s.pop()] = a[i];
             }
             s.push(i);
@@ -57,10 +52,11 @@ public class Main_17298 {
             ans[s.pop()] = -1;
         }
         
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
         for (int i=0; i<n; i++) {
             bw.write(ans[i] + " ");
         }
-        
         bw.write("\n");
         bw.flush();
     }
