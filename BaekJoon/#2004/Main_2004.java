6c34feaaf05bf72d650fb929a1dac814eb36f71e
aloha0319 <50447511+aloha0319@users.noreply.github.com>
2020-11-28 21:34:07 +0900

    20201128

---
+++ BaekJoon/#2004/Main_2004.java
@@ -0,0 +1,50 @@
+package DW_Kim_JAVA_Study_BaekJoon;
+
+import java.util.Scanner;
+
+/**
+ * 작  성  자 : Kim Do Wan
+ * 일       자 : 2020. 11. 28.
+ * 문제 제목 : 조합 0의 개수
+ * 문제 번호 : 2004
+ * 문제 출처 : https://www.acmicpc.net/
+ * 
+ * 문       제 : nCm의 끝자리 0의 개수를 출력하는 프로그램을 작성하시오.
+ *
+ * 입       력 : 첫째 줄에 정수 n, m (0 ≤ m ≤ n ≤ 2,000,000,000, n ≠ 0)이 들어온다.
+ *
+ * 출       력 : 첫째 줄에 0의 개수를 출력한다.
+ *
+ * 
+ **/
+public class Main_2004 {
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        
+        long n = sc.nextLong();
+        long m = sc.nextLong();
+        long two = 0, five = 0;
+        
+        for (long i=2; i<=n; i*=2) {
+            two += n/i;
+        }
+        for (long i=2; i<=n-m; i*=2) {
+            two -= (n-m)/i;
+        }
+        for (long i=2; i<=m; i*=2) {
+            two -= m/i;
+        }
+        for (long i=5; i<=n; i*=5) {
+            five += n/i;
+        }
+        for (long i=5; i<=n-m; i*=5) {
+            five -= (n-m)/i;
+        }
+        for (long i=5; i<=m; i*=5) {
+            five -= m/i;
+        }
+        
+        System.out.println(Math.min(two,five));
+    }
+}
